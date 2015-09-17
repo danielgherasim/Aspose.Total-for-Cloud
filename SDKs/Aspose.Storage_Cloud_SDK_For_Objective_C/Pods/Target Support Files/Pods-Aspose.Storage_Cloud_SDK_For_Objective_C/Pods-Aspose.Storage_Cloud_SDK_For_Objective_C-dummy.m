18d173bc5023effab90e58146fa43c99eca75ef9
Muhammad Sohail Ismail <msohailismail@gmail.com>
2015-09-17 09:48:11 +0500

    Aspose.Storage Cloud SDK for Objective C - Version 1.0.0

---
+++ SDKs/Aspose.Storage_Cloud_SDK_For_Objective_C/Pods/Target Support Files/Pods-Aspose.Storage_Cloud_SDK_For_Objective_C/Pods-Aspose.Storage_Cloud_SDK_For_Objective_C-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Aspose_Storage_Cloud_SDK_For_Objective_C : NSObject
+@end
+@implementation PodsDummy_Pods_Aspose_Storage_Cloud_SDK_For_Objective_C
+@end
